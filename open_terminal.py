c41a3245491fc6011dd4d2fab37482d767b0400a
rbn42 <bl100@students.waikato.ac.nz>
2016-05-04 17:38:34 +1200

    open terminal

--- open_terminal.py
+++ open_terminal.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-'''
-Open new terminal window at specified path. The path is extracted from the terminal title of bash or fish.
-'''
+# Open new terminal window at specified path.
 import sys
 import os.path
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     filename='/run/shm/open_terminal.log',
                     filemode='a')
-logging.info('start')
 logging.info(sys.argv)
 
 terminal, path = sys.argv[1:]
