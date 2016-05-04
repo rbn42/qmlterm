91f43439bf84a0d6bf26e969a5db4be328566edd
rbn42 <bl100@students.waikato.ac.nz>
2016-05-04 17:33:32 +1200

    open terminal

--- open_terminal.py
+++ open_terminal.py
@@ -5,6 +5,14 @@ Open new terminal window at specified path. The path is extracted from the termi
 '''
 import sys
 import os.path
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    filename='/run/shm/open_terminal.log',
+                    filemode='a')
+logging.info('start')
+logging.info(sys.argv)
+
 terminal, path = sys.argv[1:]
-os.chdir(path)
+if len(path) > 1:
+    os.chdir(path)
 os.system(terminal)
