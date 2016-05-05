c145e60eac9c342a0ce7efad87d8e5185fe6d37c
rbn42 <bl100@students.waikato.ac.nz>
2016-05-05 15:06:40 +1200

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
+
+sudo apt-get install build-essential qmlscene qt5-qmake qt5-default qtdeclarative5-dev qtdeclarative5-controls-plugin qtdeclarative5-qtquick2-plugin libqt5qml-graphicaleffects qtdeclarative5-dialogs-plugin qtdeclarative5-localstorage-plugin qtdeclarative5-window-plugin -y
+
 cd ~/git
 git clone https://github.com/rbn42/qmltermwidget.git
 cd ~/git/qmltermwidget
