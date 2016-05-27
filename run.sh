0e4d72ee3390a4a73bd1a29ab15e82af9b4de513
rbn42 <bl100@students.waikato.ac.nz>
2016-05-27 20:05:42 +1200

    arguments

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ qmlscene \
     -I ~/git/qmltermwidget/build \
     -I ~/git/QMLProcess/build \
     ~/git/qmlterm/qmlterm.qml \
-    -- "$1" "$2" "$3" "$4" "$5" "$6" "$7"
+    -- "$@"
 #    --transparent
