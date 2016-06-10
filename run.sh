f2fcbc083b6c1935535f5603e941e212e5998d29
rbn42 <bl100@students.waikato.ac.nz>
2016-06-10 21:23:06 +1200

    arch compatible

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 qmlscene \
+    "$@" \
     -I ~/git/qmltermwidget/build \
     -I ~/git/QMLProcess/build \
     ~/git/qmlterm/qmlterm.qml \
     --transparent \
-    -- "$@"
+
+    #-- "$@"
