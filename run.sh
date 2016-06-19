2a87e4f8ddd8da86b334db4a1e04e2ad150fa65b
rbn42 <bl100@students.waikato.ac.nz>
2016-06-19 18:42:21 +1200

    run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 
+export QML2_IMPORT_PATH=~/git/QMLProcess/build:~/git/qmltermwidget/build
+
+export QML=$(dirname "$0")/qmlterm.qml
+
 cp ~/.qmltermrc $(dirname "$0")/Configuration.qml
 
-qmlscene \
-    "$@" \
-    -I ~/git/qmltermwidget/build \
-    -I ~/git/QMLProcess/build \
-    $(dirname "$0")/qmlterm.qml \
+qmlscene "$@" $QML
+
+#    -I ~/git/qmltermwidget/build \
+#    -I ~/git/QMLProcess/build \
 
 #    --transparent
