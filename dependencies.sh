adf073aba102ae8f2e2af127ba4ce2fd23c0f4b8
rbn42 <bl100@students.waikato.ac.nz>
2016-07-14 10:52:37 +1200

    dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+#ubuntu
+#sudo apt-get install build-essential qmlscene qt5-qmake qt5-default qtdeclarative5-dev qtdeclarative5-controls-plugin qtdeclarative5-qtquick2-plugin libqt5qml-graphicaleffects qtdeclarative5-dialogs-plugin qtdeclarative5-localstorage-plugin qtdeclarative5-window-plugin -y
+
+#archlinux
+#sudo pacman -S qt5-base qt5-declarative qt5-quickcontrols qt5-graphicaleffects
