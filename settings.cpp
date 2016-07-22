0934e2db8e445c1250219bab5afd6aa86c07619e
rbn42 <bl100@students.waikato.ac.nz>
2016-07-22 18:10:00 +1200

    settings.cpp

--- settings.cpp
+++ settings.cpp
@@ -1,13 +1,16 @@
 #include "settings.h"
 
-Settings::Settings(QString path,QSettings::Format format) :
-  QSettings(path,format) {
+Settings::Settings(QString path, QSettings::Format format)
+    : QSettings(path, format)
+{
 }
 
-void Settings::setValue(const QString &key, const QVariant &value) {
-  QSettings::setValue(key, value);
+void Settings::setValue(const QString& key, const QVariant& value)
+{
+    QSettings::setValue(key, value);
 }
 
-QVariant Settings::value(const QString &key, const QVariant &defaultValue) const {
-  return QSettings::value(key, defaultValue);
+QVariant Settings::value(const QString& key, const QVariant& defaultValue) const
+{
+    return QSettings::value(key, defaultValue);
 }
